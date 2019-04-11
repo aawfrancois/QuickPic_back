e6c2f7215c511f26f1a5e15a25b4197b34852d08
aawfrancois <aawfrancois@gmail.com>
2019-04-11 17:41:21 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('hello world Express')
 })
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
