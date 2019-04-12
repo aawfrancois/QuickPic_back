e128131e40ea7465b97ad500363699ac4878fa9d
Thomas Pelfrene <pelfrene.thomas@gmail.com>
2019-04-12 09:20:37 +0200

    adding game model

---
+++ models/game.js
@@ -0,0 +1,25 @@
+import { Model } from "sequelize";
+
+export default class Game extends Model {
+  static init(sequelize, DataTypes) {
+    super.inti(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        startGame: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        endGame: {
+          type: DataTypes.DATE,
+          allowNull: false
+        },
+        // TODO 
+        // Associate model to other models
+      }
+    )
+  }
+}
