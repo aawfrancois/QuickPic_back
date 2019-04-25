3b0e1ada47b18f10c9d9795258aa8251f2964ad6
aawfrancois <aawfrancois@gmail.com>
2019-04-25 18:56:48 +0200

    add test

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,31 +4,43 @@ const server = require('../src/index');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const should = chai.should();
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
 
-describe("index2 test", () => {
-    describe("sayHello function", () => {
-        it("should say Hello guys!", () => {
+describe("Students", () => {
+    describe("GET /", () => {
+        // Test to get all students record
+        it("should get all students record", (done) => {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+        // Test to get single student record
+        it("should get a single student record", (done) => {
+            const id = 1;
+            chai.request(app)
+                .get(`/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
 
-
-            const str = sayHello();
-            expect(str).to.equal("Hello guys!")
-        })
-    })
-})
-
-describe("test routes users", () => {
-    describe("get /users", () => {
-        it("should have obj!", () => {
-            chai.request('localhost:3000/api').get('/users').end((err, res) => { // On requète la route GET
-                res.should.have.status(200); // On vérifie le statu de la réponse
-                res.body.should.be.a('object'); // On vérifie que le résultat est un tableau
-                res.body.length.should.be.eql(0); // On vérifie que la longueur du tableau est de 0
-                done(); // On dit à mocha que l'on a fini nos assertions
-            });
-
-            const str = sayHello();
-            expect(str).to.equal("Hello guys!")
-        })
-    })
-})
+        // Test to get single student record
+        it("should not get a single student record", (done) => {
+            const id = 5;
+            chai.request(app)
+                .get(`/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+    });
+});
